71b772433e9a0478c2d78aa371cdfc9d38eca23d
Yasin Amin <yasin.amin@hotmail.co.uk>
2021-11-24 17:24:56 +0000

    refactor

--- backend/views/site/pizzas.php
+++ backend/views/site/pizzas.php
@@ -1,4 +1,5 @@
 <?php
+
 use yii\helpers\Html;
 
 /* @var $this yii\web\View */
@@ -8,27 +9,27 @@ $this->title = 'My Yii Application';
 <div class="site-pizzas">
     <h1>Pizzas Page</h1>
 
-<span><?= Html::a('Create', ['/site/create'], ['class' => 'btn btn-primary']) ?></span>
+    <span><?= Html::a('Create', ['/site/create'], ['class' => 'btn btn-primary']) ?></span>
 
-<table class="table table-hover">
-  <thead>
-        <tr>
-        <th scope="col">ID</th>
-        <th scope="col">Name</th>
-        <th scope="col">Description</th>
-        <th scope="col">Actions</th>
-        </tr>
-  </thead>
-  <tbody>
-        <?php foreach($pizzas as $pizza): ?>
+    <table class="table table-hover">
+        <thead>
             <tr>
-                <th scope="row"><?= $pizza->id; ?></th>
-                <td><?= $pizza->name; ?></td>
-                <td><?= $pizza->description; ?></td>
-                <td><?= Html::a('Edit', ['edit', 'id' => $pizza->id], ['class' => 'btn btn-primary']) ?> </td>
-                <td><?= Html::a('Delete', ['delete', 'id' => $pizza->id], ['class' => 'btn btn-danger']) ?> </td>
+                <th scope="col">ID</th>
+                <th scope="col">Name</th>
+                <th scope="col">Description</th>
+                <th scope="col">Actions</th>
             </tr>
-        <?php endforeach; ?>
-    </tbody>
-</table>
+        </thead>
+        <tbody>
+            <?php foreach ($pizzas as $pizza) : ?>
+                <tr>
+                    <th scope="row"><?= $pizza->id; ?></th>
+                    <td><?= $pizza->name; ?></td>
+                    <td><?= $pizza->description; ?></td>
+                    <td><?= Html::a('Edit', ['edit', 'id' => $pizza->id], ['class' => 'btn btn-primary']) ?>
+                        <?= Html::a('Delete', ['delete', 'id' => $pizza->id], ['class' => 'btn btn-danger']) ?> </td>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
 </div>
