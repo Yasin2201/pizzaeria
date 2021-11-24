78c517b2ab09c0225004db312a8bea0e43a7044f
Yasin Amin <yasin.amin@hotmail.co.uk>
2021-11-24 14:33:48 +0000

    create signup view for backend

---
+++ backend/views/site/signup.php
@@ -0,0 +1,19 @@
+<?php
+
+/* @var $this yii\web\View */
+/* @var $form yii\bootstrap4\ActiveForm */
+/* @var $model \frontend\models\SignupForm */
+
+use yii\bootstrap4\Html;
+use yii\bootstrap4\ActiveForm;
+
+$this->title = 'Signup';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="site-signup">
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>Please fill out the following fields to signup:</p>
+
+
+</div>
