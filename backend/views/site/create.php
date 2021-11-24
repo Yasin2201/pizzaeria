d16e60a20b5f82edc86485ce776b70a1635505ef
Yasin Amin <yasin.amin@hotmail.co.uk>
2021-11-24 16:10:34 +0000

    render ActiveForm and use users inputs to submit a new pizza

--- backend/views/site/create.php
+++ backend/views/site/create.php
@@ -1,9 +1,39 @@
 <?php
+use yii\widgets\ActiveForm;
+use yii\helpers\Html;
 
 /* @var $this yii\web\View */
 
 $this->title = 'My Yii Application';
 ?>
+
 <div class="site-pizzas">
     <h1>Create Pizza</h1>
+
+    <?php $form = ActiveForm::begin() ?>
+
+        <form>
+        <fieldset>
+
+            <div class="form-group">
+                <?= 
+                    $form
+                    ->field($pizza, 'name')
+                    ->textInput(); 
+                ?>
+
+                <?= 
+                    $form
+                    ->field($pizza, 'description')
+                    ->textInput(); 
+                ?>  
+
+            </div>
+
+            <span><?= Html::submitButton('Submit', ['class' => 'btn btn-success']) ?></span>
+            <a href=<?= yii::$app->homeUrl;?> class='btn btn-danger'>Cancel</a>
+        </fieldset>
+    </form>
+
+    <?php ActiveForm::end() ?>
 </div>
