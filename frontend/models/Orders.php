a08c74a2307cd7a347dbf783912cdb1b094a17c7
Yasin Amin <yasin.amin@hotmail.co.uk>
2021-11-25 12:19:31 +0000

    create model for Orders

---
+++ frontend/models/Orders.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace frontend\models;
+
+use Yii;
+use yii\base\Model;
+use yii\db\ActiveRecord;
+
+class Orders extends ActiveRecord
+{
+    public function rules()
+    {
+        return [
+            ['first_name', 'required'],
+            ['first_name', 'string', 'min' => 2, 'max' => 255],
+
+            ['last_name', 'required'],
+            ['last_name', 'string', 'min' => 2, 'max' => 255],
+
+            ['contact_num', 'required'],
+            ['contact_num', 'integer'],
+
+            ['email', 'trim'],
+            ['email', 'required'],
+            ['email', 'email'],
+            ['email', 'string', 'max' => 255],
+
+            ['pizza_id', 'required'],
+            ['pizza_id', 'integer'],
+        ];
+    }
+}
