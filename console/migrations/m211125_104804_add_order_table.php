903fd0b1b427c9362f33d6ddc2be1fb7b238e4ea
Yasin Amin <yasin.amin@hotmail.co.uk>
2021-11-25 11:17:24 +0000

    create db table for orders

---
+++ console/migrations/m211125_104804_add_order_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m211125_104804_add_order_table
+ */
+class m211125_104804_add_order_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('orders', [
+            'id' => $this->primaryKey(),
+            'pizza_id' => $this->integer()->notNull(),
+            'first_name' => $this->string()->notNull(),
+            'last_name' => $this->string()->notNull(),
+            'contact_num' => $this->integer()->notNull(),
+            'email' => $this->string()->notNull(),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('orders');
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m211125_104804_add_order_table cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
