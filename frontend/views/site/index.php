ea5d37835b749ef90676a8f07c7667902eaecb11
Yasin Amin <yasin.amin@hotmail.co.uk>
2021-11-26 12:00:46 +0000

    redesign home page for frontend

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -1,9 +1,24 @@
 <?php
 
+use yii\helpers\Html;
+
 /* @var $this yii\web\View */
 
 $this->title = 'My Yii Application';
 ?>
 <div class="site-index">
-    <h1>Home</h1>
+    <div class="jumbotron text-center bg-transparent">
+        <h1 class="display-4">Pizzaeria</h1>
+
+        <p class="lead">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+            </br> sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </br> Ut enim ad minim veniam.
+        </p>
+        <?= Html::a('View Pizzas', ['pizzas'], ['class' => 'btn btn-primary']) ?>
+        </br>
+        <span>OR</span>
+        </br>
+        <?= Html::a('Track Order', ['trackorder'], ['class' => 'btn btn-success']) ?>
+    </div>
 </div>
