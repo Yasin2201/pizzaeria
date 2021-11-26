301dde01de880c3afd159cb15a0916d48ff07c30
Yasin Amin <yasin.amin@hotmail.co.uk>
2021-11-26 17:09:08 +0000

    render pizza and amount of times its been ordered

--- backend/views/site/dashboard.php
+++ backend/views/site/dashboard.php
@@ -6,4 +6,16 @@ $this->title = "My Yii Application";
 
 <div>
     <h1>Dashboard</h1>
+    <table>
+        <?php foreach ($pizzas as $pizza) { ?>
+            <tr>
+                <td>
+                    <?= $pizza->name ?>
+                </td>
+                <td>
+                    <?= count($pizza->orders) ?>
+                </td>
+            </tr>
+        <?php } ?>
+    </table>
 </div>
