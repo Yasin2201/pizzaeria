37fc16ded96a47a834aa1f3edc98a7cc84d670d2
Yasin Amin <yasin.amin@hotmail.co.uk>
2021-11-30 10:43:51 +0000

    create new order_items table

---
+++ console/migrations/m211130_104033_new_order_items_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m211130_104033_new_order_items_table
+ */
+class m211130_104033_new_order_items_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('order_items', [
+            'id' => $this->primaryKey(),
+            'order_ref' => $this->integer()->notNull(),
+            'item_id' => $this->integer()->notNull(),
+            'item_category' => $this->string()->notNull(),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('order_items');
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m211130_104033_new_order_items_table cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
