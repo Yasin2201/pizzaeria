1195988371cc6a8bb73a0ee3f5f1d3f941e4db13
Yasin Amin <yasin.amin@hotmail.co.uk>
2021-11-30 10:36:54 +0000

    remove order button from pizzas views

--- frontend/views/site/view.php
+++ frontend/views/site/view.php
@@ -8,6 +8,5 @@ $this->title = 'My Yii Application';
 <div class="site-view">
     <h1><?= $pizza->name ?></h1>
     <p><?= $pizza->description ?></p>
-    <?= Html::a('Order', ['order', 'id' => $pizza->id], ['class' => 'btn btn-primary']) ?>
     <?= Html::a('Add to cart', ['add-cart', 'id' => $pizza->id, 'category' => $pizza->category], ['class' => 'btn btn-primary']) ?>
 </div>
