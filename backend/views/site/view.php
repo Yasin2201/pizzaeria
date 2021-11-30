9db7800e13abec8f092d72d8f325a2f6ae32c7b2
Yasin Amin <yasin.amin@hotmail.co.uk>
2021-11-30 11:54:15 +0000

    render each item that customer has ordered with details in table

--- backend/views/site/view.php
+++ backend/views/site/view.php
@@ -7,7 +7,7 @@ $this->title = "My Yii Application";
 
 <div>
     <h1>View Order: <?= $order->id ?></h1>
-    <h3>Order Details</h3>
+    <h3>Customer Details</h3>
     <table class="table table-hover">
         <thead>
             <tr>
@@ -16,7 +16,6 @@ $this->title = "My Yii Application";
                 <th scope="col">Last Name</th>
                 <th scope="col">Contact Number</th>
                 <th scope="col">Email</th>
-                <th scope="col">Item Ordered</th>
                 <th scope="col">Order Status</th>
                 <th scope="col">Actions</th>
             </tr>
@@ -28,10 +27,42 @@ $this->title = "My Yii Application";
                 <td><?= $order->last_name; ?></td>
                 <td><?= $order->contact_num; ?></td>
                 <td><?= $order->email; ?></td>
-                <td><?= $pizza->name; ?> Pizza</td>
                 <td><?= $order->order_status; ?></td>
                 <td><?= Html::a('Edit', ['editorder', 'id' => $order->id], ['class' => 'btn btn-primary']) ?></td>
             </tr>
+        </tbody>
+
+    </table>
+
+    <h3>Order Details</h3>
+    <table class="table table-hover">
+        <thead>
+            <tr>
+                <th scope="col">ID</th>
+                <th scope="col">Item Name</th>
+                <th scope="col">Item Description</th>
+                <th scope="col">Item Category</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($orderedItems as $item) { ?>
+                <tr>
+                    <th><?= $item->item_id; ?></th>
+
+                    <?php if ($item->item_category == "Pizza") { ?>
+                        <td><?= $item->pizzas->name; ?></td>
+                        <td><?= $item->pizzas->description; ?></td>
+                    <?php } elseif ($item->item_category == "Side") { ?>
+                        <td><?= $item->sides->name; ?></td>
+                        <td><?= $item->sides->description; ?></td>
+                    <?php } else { ?>
+                        <td><?= $item->toppings->name; ?></td>
+                        <td><?= $item->toppings->description; ?></td>
+                    <?php } ?>
+
+                    <td><?= $item->item_category; ?></td>
+                </tr>
+            <?php } ?>
         </tbody>
     </table>
 </div>
